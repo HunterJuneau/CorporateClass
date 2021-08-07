4d41d6ff31c3f131b69691128066881ac1e1df8b
Hunter Juneau <huntlee17@gmail.com>
2021-08-07 10:30:44 -0500

    Add project files.

---
+++ CorporateClass/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorporateClass
+{
+    public class Employee
+    {
+        public string Name { get; private set; }
+        public string Title { get; private set; }
+        public DateTime StartDate { get; }
+
+        public Employee(string name, string title, DateTime startDate)
+        {
+            Name = name;
+            Title = title;
+            StartDate = startDate;
+        }
+
+        public void ChangeName(string newName)
+        {
+            Name = newName;
+        }
+
+        public void ChangePosition(string newTitle)
+        {
+            Title = newTitle;
+        }
+    }
+}
---
+++ CorporateClass/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CorporateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Company UniversalPaperclips = new Company("Universal Paperclips", DateTime.Now);
+
+            Employee Ben = new Employee("Ben", "CEO", DateTime.Now);
+            UniversalPaperclips.AddEmployee(Ben);
+
+            Employee Martin = new Employee("Martin", "Supervisor", DateTime.Now);
+            UniversalPaperclips.AddEmployee(Martin);
+
+            Employee Jonathan = new Employee("Jonathan", "Paperclip Forge Operator", DateTime.Now);
+            UniversalPaperclips.AddEmployee(Jonathan);
+
+            UniversalPaperclips.ListEmployees();
+
+
+            Company ApertureScience = new Company("Aperture Science Laboratories Computer-Aided Enrichment Center", DateTime.Now);
+
+            Employee Chie = new Employee("Chie", "CEO", DateTime.Now);
+            ApertureScience.AddEmployee(Chie);
+
+            Employee HoneyRae = new Employee("Honey-Rae", "Supervisor", DateTime.Now);
+            ApertureScience.AddEmployee(HoneyRae);
+
+            Employee Casey = new Employee("Casey", "Test Subject", DateTime.Now);
+            ApertureScience.AddEmployee(Casey);
+
+            ApertureScience.ListEmployees();
+        }
+    }
+}
